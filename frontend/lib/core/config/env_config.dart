8efa528c4c80a987dcc0f8ef530b19304c10bd04
Alba  Gomez <albaliriagomezmontano0419@gmail.com>
2026-03-16 14:49:11 -0400

    feat: pantalla de mapa terminada y actualizada con dev

--- frontend/lib/core/config/env_config.dart
+++ frontend/lib/core/config/env_config.dart
@@ -1,7 +1,7 @@
 class EnvConfig {
   // PARA LOCAL: Usa tu IP local
   // PARA PRODUCCIÓN: Usa tu dominio 'https://tu-dominio.com'
-  static const String baseUrl = "http://172.16.51.155:8000"; 
+  static const String baseUrl = "http://192.168.0.15:8000"; 
   
   static const String apiBaseUrl = "$baseUrl/api";
 }
